9cb60f746e5a07a7ff1bd68b4149e9b0c758b106
Sivadej K <bombyk@gmail.com>
2020-02-27 15:27:12 -0900

    functioning game without timer and dupe check

--- static/boggle.js
+++ static/boggle.js
@@ -1,23 +1,15 @@
 document.getElementById('submit').addEventListener('click', function (e) {
     e.preventDefault();
-    //alert('click!');
 });
 
-
-
-// $('#submit').click(function () {
-//     $word = $('#answer').val()
-//     $response = $.getJSON({
-//         url: '/check-answer',
-//         type: 'GET',
-//         data: {
-//             answer: $word
-//         }
-//     });
-//     alert($response);
-// })
+$('#your-guess-line').hide();
+$('#score-line').hide();
+$('#answer').focus();
+let score = 0;
 
 $('#submit').click(function () {
+    $('#your-guess-line').show();
+    $('#score-line').show();
     $word = $('#answer').val()
     $response = $.ajax({
         url: '/check-answer',
@@ -26,16 +18,30 @@ $('#submit').click(function () {
         dataType: 'json'
     }).done(function (data) {
         console.log(data);
-    })
+        displayResult(data.result);
+    });
+});
 
-})
+function displayResult(result) {
+    $('#your-guess').text($('#answer').val().toUpperCase());
+    let resultHTML = ''
+    switch (result) {
+        case "not-on-board":
+            resultHTML = 'That word is not on this board!';
+            break;
+        case "ok":
+            resultHTML = 'You got it!';
+            score += 1;
+            updateScoreDisplay();
+            break;
+        case "not-word":
+            resultHTML = `That's not even a word!`;
+            break;
+    }
+    $('#guess-result').text(resultHTML);
+    $('#answer').val('').focus();
+}
 
-// $('#submit').click(function () {
-//     $.ajax({
-//         method: 'GET',
-//         url: 'http://jsonplaceholder.typicode.com/posts',
-//         dataType: 'json'
-//     }).done(function (data) {
-//         console.log(data);
-//     });
-// })
+function updateScoreDisplay() {
+    $('#score').text(score);
+}
